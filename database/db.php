5eff407e12451e43dc98b35e23d9407d6fffdeb3
habray-99 <vamsha.tamu.a21.2@icp.edu.np>
2024-06-22 13:22:20 +0545

    chore: Add initial database structure and connection code

---
+++ database/db.php
@@ -0,0 +1,15 @@
+<?php
+require_once ('config.php');
+
+$host = "localhost";
+$db_name = "ride_nepal";
+$username = "root";
+$password = "";
+
+// Database Connection
+$conn = new mysqli($host, $username, $password, $db_name);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
