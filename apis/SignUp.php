4fc2e1236cd7e1ddfcad887595977881ecf56d2e
habray-99 <vamsha.tamu.a21.2@icp.edu.np>
2024-06-22 20:26:36 +0545

    feat: Add API endpoint for user signup
    This commit adds a new PHP file, SignUp.php, which contains the code for handling user signup requests. The file includes necessary headers for handling CORS and sets up the required database connection. It also includes logic for validating and inserting user credentials into the database. The purpose of this commit is to provide an API endpoint for users to sign up, allowing them to create an account with their full name, username, phone number, email, and password.

---
+++ apis/SignUp.php
@@ -0,0 +1,53 @@
+<?
+require_once('../database/db.php');
+header('Content-Type: application/json');
+// Handle form submission
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    
+    if (!isset($fullName) || !isset($userName) || !isset($phone) || !isset($email) || !isset($password)) {
+        // Redirect to the signup page with an error message
+        $response = array(
+            'status' => 'error',
+            'message' => 'Incomplete credentials'
+        );
+        exit();
+    }
+    // Retrieve the user details from the form
+    $fullName = $_POST['fullName'];
+    $userName = $_POST['userName'];
+    $phone = $_POST['phone'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    // Encrypt the password
+    $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+
+    // Insert the new user into the database
+    $sql = "INSERT INTO users (fullName, userName, phone, email, password) VALUES ('$fullName','$userName','$phone', '$email', '$hashedPassword')";
+    $conn->query($sql);
+
+    $user_id = $conn->insert_id; // Get the ID of the newly inserted user
+
+    // Generate a token
+    $token = bin2hex(random_bytes(16));
+
+    // Store the token in the api_tokens table
+    $tokenSql = "INSERT INTO api_tokens (user_id, token) VALUES ($user_id, '$token')";
+    $conn->query($tokenSql);
+
+    $sql2 = "SELECT * FROM users WHERE id = '$user_id'";
+    // Execute the SQL statement
+    $result2 = $conn->query($sql2);
+
+    $row2 = $result2->fetch_assoc();
+
+    $response = array(
+        'status' => 'success',
+        'message' => 'Login successful',
+        'token' => $token,
+        'user' => $row2,
+    );
+
+    // Redirect to the dashboard page
+    // header("Location: users.php");
+    exit();
+}
